0c80c84ba03312398eae6fa4b8762bf3077d5899
Hamid_mp <mohamadali.mahmoodpour@gmail.com>
2024-01-16 20:57:07 +0330

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 from tqdm import tqdm
 import torch.nn as nn
 from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 from models import CreateModel
 from torchvision import transforms as tfs
 from loss import FocalLoss
@@ -9,28 +10,25 @@ BATCH_SIZE = 32
 EPOCHS = 100
 LR = 0.01
 
-tfs.Compose([
-    tfs.RandomCrop((112, 112)),
+transforms = tfs.Compose([
     tfs.RandomVerticalFlip(0.5),
     tfs.RandomHorizontalFlip(p=0.5),
     tfs.RandomGrayscale(0.3),
-    tfs.RandomCrop(),
+    tfs.Resize((224,224)),
     tfs.ToTensor(),
     
 ])
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = CreateModel('resnet18.a1_in1k', 80).load_model()
+model = CreateModel('resnet18.a1_in1k', 84).load_model()
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 criterion = FocalLoss()
 
 
 
 
-
-
-
-
+train_set = ImageFolder(root='./FlagCrops', transform=transforms)
+train_loader = DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
 def train(model,
           train_loader,
           valid_loader,
@@ -64,27 +62,36 @@ def train(model,
 
     train_loss /= len(train_loader)
     train_acc /= len(train_loader)
-
-    with torch.inference_mode():
-        valid_loss, valid_acc = 0, 0
-
-        for x, y in valid_loader:
-
-            x, y = x.to(device), y.to(device)
-
-            y_pred = model(x)
-
-            loss = criterion(y_pred, y)
-
-            valid_loss += loss
-
-            valid_acc = (torch.eq(y_pred.argmax(dim=1), y).sum().item() / len(y_pred)) * 100
-
-        valid_acc /= len(valid_loader)
-        valid_loss /= len(valid_loader)
     print(f'-------[{epoch+1}| {epochs}] --------')
-    print(f"Train Acc: {train_acc} \t Train Loss: {train_loss} \n 
-          Valid Acc: {valid_acc} \t Valid Loss: {valid_loss}\n")
+    print(f"Train Acc: {train_acc} \t Train Loss: {train_loss}")
+    if valid_loader:
+        with torch.inference_mode():
+            valid_loss, valid_acc = 0, 0
 
+            for x, y in valid_loader:
+
+                x, y = x.to(device), y.to(device)
+
+                y_pred = model(x)
+
+                loss = criterion(y_pred, y)
+
+                valid_loss += loss
+
+                valid_acc = (torch.eq(y_pred.argmax(dim=1), y).sum().item() / len(y_pred)) * 100
+
+            valid_acc /= len(valid_loader)
+            valid_loss /= len(valid_loader)
+            print(f"Valid Acc: {valid_acc} \t Valid Loss: {valid_loss}\n")
+
+
+if __name__ == '__main__':
+    train(model,
+          train_loader,
+          None,
+          EPOCHS,
+          optimizer,
+          criterion,
+          device)
 
 
